004a6f14c387ba001243acfed092e0d002f3aaf6
Your Name <you@example.com>
2018-02-27 15:14:01 +0800

    部分静态页面

--- my_project/routes/index.js
+++ my_project/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+  res.render('login', { title: 'ECSHOP登录中心' });
+});
+router.get('/back', function(req, res, next) {
+  res.render('back', { title: 'ECSHOP管理中心' });
 });
 
 module.exports = router;
